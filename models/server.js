1a32eccc80fe3f3634ae2b650a974f517a06179b
José Lenner Picado <141876655+Lennwmedia@users.noreply.github.com>
2024-11-19 09:39:06 -0600

    Subi un puerto predeterminado en caso que no corra su variable de entorno

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 class Server {
 
     constructor() {
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.app = express();
         this.usuariosPath = '/api/usuarios'
 
