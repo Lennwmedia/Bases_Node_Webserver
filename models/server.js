6d98fd0aa6f22fabc4234c53d113735112ba5ead
Lennwmedia <lennwmedia@gmail.com>
2024-11-22 08:16:22 -0600

    Fin de la sección 9

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
 
@@ -8,11 +9,18 @@ class Server {
         this.app = express();
         this.usuariosPath = '/api/usuarios'
 
+        //Conectar database
+        this.conectarDB();
+
         // Middlewares 
         this.middlewares();
 
         // Rutas
         this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
     }
 
     middlewares() {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { esRolValido, emailExiste, usuarioExistePorId } = require('../helpers/db-validators')
+const { validarCampos } = require('../middlewares/validar-campos');
 const { 
     usuariosGet, 
     usuariosPut, 
@@ -10,12 +14,28 @@ const router = Router();
 
 router.get('/', usuariosGet);
 
-router.put('/:id', usuariosPut);
+router.put('/:id', [
+    check('id', 'El ID no es válido').isMongoId(),
+    check('id').custom(usuarioExistePorId),
+    check('rol').custom( esRolValido ),
+    validarCampos
+], usuariosPut);
 
-router.post('/', usuariosPost);
+router.post('/', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 }),
+    check('email', 'El correo no es valido').isEmail(),
+    check('email').custom( emailExiste ),
+    check('rol').custom( esRolValido ),
+    validarCampos,
+], usuariosPost);
 
 router.patch('/', usuariosPatch);
 
-router.delete('/', usuariosDelete);
+router.delete('/:id', [
+    check('id', 'El ID no es válido').isMongoId(),
+    check('id').custom(usuarioExistePorId),
+    validarCampos
+], usuariosDelete);
 
 module.exports = router
