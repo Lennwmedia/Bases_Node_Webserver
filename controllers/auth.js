cb77b627c8da112dadc00e081ac1b9268cd1e1f8
Lennwmedia <lennwmedia@gmail.com>
2024-11-28 09:41:40 -0600

    Fin sección 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req = request, res = response) => {
 
@@ -51,6 +52,53 @@ const login = async(req = request, res = response) => {
     }
 }
 
+const googleSignIn = async(req = request, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { name, img, email } = await googleVerify( id_token );
+        
+        let usuario = await Usuario.findOne({ email });
+
+         if ( !usuario ) {
+            // crear usuario
+            const data = {
+                name,
+                email,
+                password: ':b',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+            }
+ 
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if ( !usuario.state ) {
+            return res.status(401).json({
+                msg: 'Hable con el adminsitrador, usuario bloqueado'
+            })
+        }  
+
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            msg: 'todo bien!',
+            usuario,
+            token
+        }) 
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido',
+        })
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
