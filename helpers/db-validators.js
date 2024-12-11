8edd56fdfdc46e17068512f374c6ea23bb7e2979
Lennwmedia <lennwmedia@gmail.com>
2024-12-11 11:55:43 -0600

    Fin sección 13 - version 6.0.0

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -40,10 +40,21 @@ const existeProductoPorId = async( id = '' ) => {
     }
 }
 
+const coleccionValida = (coleccion = '', colecciones = []) => {
+    const incluida = colecciones.includes(coleccion);
+
+    if (!incluida) {
+        throw new Error(`La coleccion ${coleccion} no es permitida - ${colecciones}`);   
+    }
+
+    return true;
+}
+
 module.exports = {
     esRolValido,
     emailExiste,
     usuarioExistePorId,
     existeCategoriaPorId,
-    existeProductoPorId
+    existeProductoPorId,
+    coleccionValida
 }
--- models/server.js
+++ models/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const { dbConnection } = require('../database/config');
+
 
 class Server {
 
@@ -13,6 +15,7 @@ class Server {
             buscar: '/api/buscar',
             categorias: '/api/categorias',
             productos: '/api/productos',
+            uploads: '/api/uploads',
             usuarios: '/api/usuarios',
         }
 
@@ -40,6 +43,13 @@ class Server {
 
         //Leer y Parsear data
         this.app.use( express.json() )
+
+        // Cargar Archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes() {
@@ -47,6 +57,7 @@ class Server {
         this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
     }
 
